c6ccf628b2af3bde5e627e85255f4d578fa96b55
rajahwu <vmradford@gmail.com>
2022-02-07 22:22:01 -0500

    start game feature

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-import $ from "https://cdn.skypack.dev/jquery@3.6.0";
+// import $ from "https://cdn.skypack.dev/jquery@3.6.0";
 
-var addBtn = $("#addItemBtn");
+// var addBtn = $("#addItemBtn");
 
-$("button").on("click", function () {
-  let itemName = $(#itemName);
-  let itemPrice = $('#itemPrice');
-  let itemQuantity = $('#itemQuantity');
-  console.log(itemName.value);
-  console.log(itemPrice.value);
-  console.log(itemQuantity.value);
+// $("button").on("click", function () {
+//   let itemName = $(#itemName);
+//   let itemPrice = $('#itemPrice');
+//   let itemQuantity = $('#itemQuantity');
+//   console.log(itemName.value);
+//   console.log(itemPrice.value);
+//   console.log(itemQuantity.value);
 
-  $("#itemList").html(itemName.value);
-  $("#itemList").html(itemPrice.value);
-  $("#itemList").html(itemQuantity.value);
-  console.log(itemName.value);
-  console.log(itemPrice.value);
-  console.log(itemQuantity.value);
-});
+//   $("#itemList").html(itemName.value);
+//   $("#itemList").html(itemPrice.value);
+//   $("#itemList").html(itemQuantity.value);
+//   console.log(itemName.value);
+//   console.log(itemPrice.value);
+//   console.log(itemQuantity.value);
+// });
 
 // var addBtm = document.getElementById('addItemBtn')
 // function listItem() {
