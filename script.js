ab944a3a3f091e53b6c203a505c52eb505d37d1a
rajahwu <vmradford@gmail.com>
2022-02-08 15:48:23 -0500

    create var for price tax and totals

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 
 // create variables to capture user input
 var itemName = document.getElementById('itemName');
-console.log(itemName);
+//console.log(itemName);
 var itemPrice = document.getElementById('itemPrice');
-console.log(itemPrice);
+//console.log(itemPrice);
 var itemQuantity = document.getElementById('itemQuantity');
-console.log(itemQuantity);
+//console.log(itemQuantity);
 
 // create variable to display user input
 let itemDisplay = itemList.querySelector('.item')
-console.log(itemDisplay)
+//console.log(itemDisplay)
 let priceDisplay =  itemList.querySelector('.price')
-console.log(priceDisplay)
+//console.log(priceDisplay)
 let subtotalDisplay = itemList.querySelector('.subtotal')
+//console.log(subtotalDisplay)
 let taxDisplay = itemList.querySelector('.tax')
-console.log(taxDisplay)
+//console.log(taxDisplay)
 let totalDisplay = itemList.querySelector('.total')
-console.log(totalDisplay)
+//console.log(totalDisplay)
+
+
+
+  let subtotal = itemPrice.value * itemQuantity.value
+  let tax = subtotal * 0.06
+  let total = subtotal + tax
+  console.log(subtotal, tax, total)
 
 
 // add event listeners to add item button
