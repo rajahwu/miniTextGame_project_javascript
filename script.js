b2f60c25f1347356d6c2321bc8ff97af488ce3e1
rajahwu <vmradford@gmail.com>
2022-02-08 15:29:35 -0500

    display user input in window

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 
-
+// create variables to capture user input
 var itemName = document.getElementById('itemName');
 console.log(itemName);
-
 var itemPrice = document.getElementById('itemPrice');
 console.log(itemPrice);
-
 var itemQuantity = document.getElementById('itemQuantity');
 console.log(itemQuantity);
 
+// create variable to display user input
+let itemDisplay = itemList.querySelector('.item')
+console.log(itemDisplay)
+let priceDisplay =  itemList.querySelector('.price')
+console.log(priceDisplay)
+let subtotalDisplay = itemList.querySelector('.subtotal')
+let taxDisplay = itemList.querySelector('.tax')
+console.log(taxDisplay)
+let totalDisplay = itemList.querySelector('.total')
+console.log(totalDisplay)
+
+
+// add event listeners to add item button
 document.getElementById('addItemBtn').addEventListener('click', e => {
   console.log(itemName.value)
+  itemDisplay.textContent = itemName.value
+
 })
 
 document.getElementById('addItemBtn').addEventListener('click', e => {
   console.log(itemPrice.value)
+  priceDisplay.textContent = itemPrice.value
 })
 
 document.getElementById('addItemBtn').addEventListener('click', e => {
   console.log(itemQuantity.value)
+  totalDisplay.textContent = itemQuantity.value
 })
