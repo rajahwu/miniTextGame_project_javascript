88c675ed7d934589945a1934b17f45286b9ae90d
rajahwu <vmradford@gmail.com>
2022-02-07 21:42:41 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+import $ from "https://cdn.skypack.dev/jquery@3.6.0";
+
+var addBtn = $("#addItemBtn");
+
+$("button").on("click", function () {
+  let itemName = $(#itemName);
+  let itemPrice = $('#itemPrice');
+  let itemQuantity = $('#itemQuantity');
+  console.log(itemName.value);
+  console.log(itemPrice.value);
+  console.log(itemQuantity.value);
+
+  $("#itemList").html(itemName.value);
+  $("#itemList").html(itemPrice.value);
+  $("#itemList").html(itemQuantity.value);
+  console.log(itemName.value);
+  console.log(itemPrice.value);
+  console.log(itemQuantity.value);
+});
+
+// var addBtm = document.getElementById('addItemBtn')
+// function listItem() {
+//   var name = document.getElementById(itemName);
+//   var price = document.getElementById(itemPrice);
+//   var quanity = document.getElementById(itemQuantity);
+//   console.log('added');
+//   console.log(itemName);
+// }
